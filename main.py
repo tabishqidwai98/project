0eb20b587d29e55c3a7ae79a9fb36d2f4ef1df44
Tabish Qidwai <60247314+tabishqidwai98@users.noreply.github.com>
2024-07-20 13:31:29 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+
+c = "gfdeyg"
+
+d = True
+
+def on_forever():
+    basic.show_number(a+b)
+    basic.show_string("hellu!")
+    
+    if d is True:
+        basic.show_number(a+a)
+    else:
+        basic.show_number(a+b)
+    
+    basic.show_number(a+b+a)
+
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,15 @@
+let a = 1
+let b = 2
+let c = "gfdeyg"
+let d = true
+basic.forever(function on_forever() {
+    basic.showNumber(a + b)
+    basic.showString("hellu!")
+    if (d === true) {
+        basic.showNumber(a + a)
+    } else {
+        basic.showNumber(a + b)
+    }
+    
+    basic.showNumber(a + b + a)
+})
